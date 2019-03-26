4e041900ce5156e37ef947766adec5f564f72265
Hristo Kamenov <hkamenov123@gmail.com>
2019-03-26 10:14:29 +0100

    refactor: Converted class to namespace with functions

--- AngleWeightedValue.h
+++ AngleWeightedValue.h
@@ -3,39 +3,34 @@
 
 #include <math.h>
 
-class AngleWeightedValue {
-  public:
+namespace AngleWeightedValue {
 
-    AngleWeightedValue() {}
-
-    float getWeightedValue(float alpha, float beta, float value)
-    {
-      float ratioX = getAngleRatioX(alpha, beta);
-      float ratioY = getAngleRatioY(alpha, beta);
-      float ratio = ratioX + ratioY;
-      return ((ratio * value) + value) / 2;
-    }
-
-  private:
-
-    float getComponentX(float alpha)
+    float _getComponentX(float alpha)
     {
       return cos(alpha * M_PI / 180);
     }
 
-    float getComponentY(float alpha)
+    float _getComponentY(float alpha)
     {
       return sin(alpha * M_PI / 180);
     }
 
-    float getAngleRatioX(float alpha, float beta)
+    float _getAngleRatioX(float alpha, float beta)
     {
-      return getComponentX(alpha) * getComponentX(beta);
+      return _getComponentX(alpha) * _getComponentX(beta);
     }
 
-    float getAngleRatioY(float alpha, float beta)
+    float _getAngleRatioY(float alpha, float beta)
     {
-      return getComponentY(alpha) * getComponentY(beta);
+      return _getComponentY(alpha) * _getComponentY(beta);
+    }
+
+    float getWeightedValue(float alpha, float beta, float value)
+    {
+      float ratioX = _getAngleRatioX(alpha, beta);
+      float ratioY = _getAngleRatioY(alpha, beta);
+      float ratio = ratioX + ratioY;
+      return ((ratio * value) + value) / 2;
     }
 };
 
